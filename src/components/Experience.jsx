e55b99ef4600f9207b8ec318081a442a45468629
Inna <innazh1133@gmail.com>
2021-05-15 00:46:56 +0300

    experience ready even though overflow-y seems to be major but it'll have to do for now

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,7 +10,6 @@ const Experience = () => {
                 <div className="col">
                     <h2 style={{ fontSize: '60px' }}>Experience</h2>
                     <div className="row">
-
                         <div className="col-lg-7 mx-auto" style={{ color: 'black' }}>
                             <ul className="timeline">
                                 <div className="row">
@@ -18,21 +17,31 @@ const Experience = () => {
                                         {/* 3 rows below for 3 job experiences */}
                                         <div className="row">
                                             <div className="col">
-
                                                 <li className="ul-li-child">
+                                                    <div class="accordion" id="morno-exp">
+                                                        <div class="accordion-item">
+                                                            <h2 class="accordion-header" id="mornoHeading">
+                                                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#morno-tasks" aria-expanded="true" aria-controls="morno-tasks">
 
-                                                    <div className="card">
-                                                        <div className="row">
-                                                            <div className="col-md-2 logo-container">
-                                                                <img className="card-img company-logo" src={morno} />
-                                                            </div>
-                                                            <div className="col-md-10">
-                                                                <div className="card-body">
-                                                                    <h5 className="card-title">Automation Test Engineer (Co-op) @ <a className="link-dark" href="https://www.morneaushepell.com/ca-en">Morneau Shepell</a></h5>
-                                                                    <h6 className="card-subtitle mb-2 text-muted">05/2020 - 08/2020</h6>
-                                                                    <p className="card-text tasks">
-                                                                        <b>Technology:</b> C#, SQL, Selenium, Appium, BrowserStack, Azure DevOps, Swagger, Postman
-                                                            </p>
+                                                                    <div className="row">
+                                                                        <div className="col-md-2 logo-container">
+                                                                            <img className="card-img company-logo" src={morno} />
+                                                                        </div>
+                                                                        <div className="col-md-9">
+                                                                            <div className="card-body">
+                                                                                <h5 className="card-title">Automation Test Engineer (Co-op) @ <a className="link-dark" href="https://www.morneaushepell.com/ca-en">Morneau Shepell</a></h5>
+                                                                                <h6 className="card-subtitle mb-2">Remote</h6>
+                                                                                <h6 className="card-subtitle mb-2 text-muted">05/2020 - 08/2020</h6>
+                                                                                <p className="card-text tasks">
+                                                                                    <b>Technology:</b> C#, SQL, Selenium, Appium, BrowserStack, Azure DevOps, Swagger, Postman
+                                                                                </p>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </button>
+                                                            </h2>
+                                                            <div id="morno-tasks" class="accordion-collapse collapse" aria-labelledby="mornoHeading" data-bs-parent="#morno-exp">
+                                                                <div class="accordion-body">
                                                                     <ul className="tasks">
                                                                         <li>Automated tests mobile application for Android using Selenium(C#), Appium and BrowserStack</li>
                                                                         <li>Used Swagger and Postman to automate regression testing for API services</li>
@@ -42,37 +51,38 @@ const Experience = () => {
                                                                 </div>
                                                             </div>
                                                         </div>
-
-
-
                                                     </div>
-
                                                 </li>
-
                                             </div>
                                         </div>
+                                        {/* OLG */}
                                         <div className="row">
                                             <div className="col">
                                                 <li className="ul-li-child">
-                                                    <div className="card">
-                                                        {/* TODO: resolve, the image needs to be lower */}
-                                                        <div className="row">
-                                                            <div className="col-md-2 logo-container">
-                                                                <div className="row">
-                                                                    <div className="col"></div>
-                                                                    <div className="col">
-                                                                <img className="card-img company-logo" src={olg} />
+                                                    <div class="accordion" id="olg-exp">
+                                                        <div class="accordion-item">
+                                                            <h2 class="accordion-header" id="olgHeading">
+                                                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#olg-tasks" aria-expanded="true" aria-controls="olg-tasks">
+
+                                                                    <div className="row">
+                                                                        <div className="col-md-2 logo-container">
+                                                                            <img className="card-img company-logo" src={olg} />
+                                                                        </div>
+                                                                        <div className="col-md-9">
+                                                                            <div className="card-body">
+                                                                                <h5 className="card-title">Business Systems Analyst (Co-op) @ <a className="link-dark" href="https://www.olg.ca/en/home.html">OLG</a></h5>
+                                                                                <h6 className="card-subtitle mb-2">Toronto, ON</h6>
+                                                                                <h6 className="card-subtitle mb-2 text-muted">05/2019 - 08/2019</h6>
+                                                                                <p className="card-text tasks">
+                                                                                    <b>Technology:</b> ServiceNow, AngularJS, Jelly, HTML, CSS, Powershell, Python, Pandas
+                                                                                </p>
+                                                                            </div>
+                                                                        </div>
                                                                     </div>
-                                                                    <div className="col"></div>
-                                                                </div>
-                                                            </div>
-                                                            <div className="col-md-10">
-                                                                <div className="card-body">
-                                                                    <h5 className="card-title">Business Systems Analyst (Co-op) @ <a className="link-dark" href="https://www.olg.ca/en/home.html">OLG</a></h5>
-                                                                    <h6 className="card-subtitle mb-2 text-muted">05/2019 - 08/2019</h6>
-                                                                    <p className="card-text tasks">
-                                                                        <b>Technology:</b> ServiceNow, AngularJS, Jelly, HTML, CSS, Powershell, Python, Pandas
-                                                            </p>
+                                                                </button>
+                                                            </h2>
+                                                            <div id="olg-tasks" class="accordion-collapse collapse" aria-labelledby="olgHeading" data-bs-parent="#olg-exp">
+                                                                <div class="accordion-body">
                                                                     <ul className="tasks">
                                                                         <li>Started a project that automated top 30 requests for the IT Support Centre using ServiceNow and AngularJS</li>
                                                                         <li>Transformed the design of the internal IT portal using AngularJS, Jelly, HTML and CSS</li>
@@ -83,28 +93,39 @@ const Experience = () => {
                                                                 </div>
                                                             </div>
                                                         </div>
-
                                                     </div>
                                                 </li>
                                             </div>
                                         </div>
+                                        {/* DMA */}
                                         <div className="row">
                                             <div className="col">
                                                 <li className="ul-li-child">
-                                                    <div className="card">
+                                                    <div class="accordion" id="dma-exp">
+                                                        <div class="accordion-item">
+                                                            <h2 class="accordion-header" id="dmaHeading">
+                                                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#dma-tasks" aria-expanded="true" aria-controls="dma-tasks">
 
-                                                        <div className="row">
-                                                            <div className="col-md-2 logo-container">
-                                                                <img className="card-img company-logo" src={dma} />
-                                                            </div>
-                                                            <div className="col-md-10">
-                                                                <div className="card-body">
-                                                                    <h5 className="card-title">Teaching Assistant @ <a className="link-dark" href="https://digitalmediaacademy.org/">Digital Media Academy</a></h5>
-                                                                    <h6 className="card-subtitle mb-2 text-muted">06/2018 - 07/2018</h6>
+                                                                    <div className="row">
+                                                                        <div className="col-md-2 logo-container">
+                                                                            <img className="card-img company-logo" src={dma} />
+                                                                        </div>
+                                                                        <div className="col-md-9">
+                                                                            <div className="card-body">
+                                                                                <h5 className="card-title">Teaching Assistant @ <a className="link-dark" href="https://digitalmediaacademy.org/">Digital Media Academy</a></h5>
+                                                                                <h6 className="card-subtitle mb-2">Toronto, ON</h6>
+                                                                                <h6 className="card-subtitle mb-2 text-muted">06/2018 - 07/2018</h6>
 
-                                                                    <p className="card-text tasks"><b>Technology:</b> Java, Processing
+                                                                                <p className="card-text tasks"><b>Technology:</b> Java, Processing
                                                                         <br />
-                                                                        <b>Other skills:</b> teamwork, great communication skills, debugging</p>
+                                                                                    <b>Other skills:</b> teamwork, great communication skills, debugging</p>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </button>
+                                                            </h2>
+                                                            <div id="dma-tasks" class="accordion-collapse collapse" aria-labelledby="dmaHeading" data-bs-parent="#dma-exp">
+                                                                <div class="accordion-body">
                                                                     <ul className="tasks">
                                                                         <li>Taught children and teens how to program utilising great communication skills</li>
                                                                         <li>Built project examples using Processing and Java</li>
